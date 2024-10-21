0dc329f5b3936fa5d89198a5cf1c5cbc9259f459
JinxYTW <tyohann974@gmail.com>
2024-10-21 14:17:25 +0200

    Le NEAT a été mis à jour afin de fonctionner avec le labyrinthe normalement.Il y a quelques méthodes à créer afin de récupérer les informations nécessaires.Je te les ai mis en TODO

--- TestImplementNEAT/GenomeIndexer.cpp
+++
@@ -1,7 +0,0 @@
-#include "GenomeIndexer.h"
-
-GenomeIndexer::GenomeIndexer() : current_index(0) {}
-
-int GenomeIndexer::next() {
-    return current_index++;  // Renvoie l'index actuel et l'incrémente
-}
